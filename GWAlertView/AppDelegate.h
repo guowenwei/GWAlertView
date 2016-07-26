b17782088ac370489ec68d21c420e025d5ac2fe9
guowenwei <guowen_wei@163.com>
2016-07-26 15:56:09 +0800

    Initial Commit

---
+++ GWAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GWAlertView
+//
+//  Created by 魏郭文 on 16/7/26.
+//  Copyright © 2016年 魏郭文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GWAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GWAlertView
+//
+//  Created by 魏郭文 on 16/7/26.
+//  Copyright © 2016年 魏郭文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
